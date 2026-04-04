38c123a5cc411d5e2a86c516430ee6b8c53a777b
Ahmed Hazem <ah1302609@gmail.com>
2026-04-04 14:05:29 +0200

    Adding yml files and manage sql files

---
+++ dbt_project/models/staging/stg_credit_card_balance.sql
@@ -0,0 +1,24 @@
+SELECT
+    SK_ID_PREV as prev_bureau_id,
+    SK_ID_CURR as loan_id,
+    MONTHS_BALANCE as months_balance,
+    CASE
+        WHEN AMT_BALANCE < 0 THEN 0
+        WHEN AMT_BALANCE > AMT_CREDIT_LIMIT_ACTUAL THEN AMT_CREDIT_LIMIT_ACTUAL
+        ELSE AMT_BALANCE
+    END as credit_card_balance,
+    AMT_CREDIT_LIMIT_ACTUAL as credit_card_limit,
+    CASE
+        WHEN AMT_DRAWINGS_CURRENT < 0 THEN 0
+        WHEN AMT_DRAWINGS_CURRENT > AMT_CREDIT_LIMIT_ACTUAL THEN AMT_CREDIT_LIMIT_ACTUAL
+        ELSE AMT_DRAWINGS_CURRENT
+    END as drawings_current,
+    AMT_PAYMENT_TOTAL_CURRENT as credit_payment_current,
+    CASE
+        WHEN AMT_TOTAL_RECEIVABLE < 0 THEN 0
+        WHEN AMT_TOTAL_RECEIVABLE > AMT_CREDIT_LIMIT_ACTUAL THEN AMT_CREDIT_LIMIT_ACTUAL
+        ELSE AMT_TOTAL_RECEIVABLE
+    END as total_receivable,
+    NAME_CONTRACT_STATUS as contract_status,
+    SK_DPD_DEF as credit_days_past_due
+FROM {{ source('raw', 'credit_card_balance') }}
---
+++ dbt_project/models/staging/stg_installments_payments.sql
@@ -0,0 +1,10 @@
+SELECT
+    SK_ID_PREV as prev_bureau_id,
+    SK_ID_CURR as loan_id,
+    NUM_INSTALMENT_VERSION as installment_version,
+    NUM_INSTALMENT_NUMBER as installment_number,
+    DAYS_INSTALMENT as days_installment,
+    DAYS_ENTRY_PAYMENT as days_actual_payment,
+    AMT_INSTALMENT as instalment_amount,
+    AMT_PAYMENT as actual_payment
+FROM {{ source('raw', 'installments_payments') }}
